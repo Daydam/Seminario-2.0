2dd5b8e6d5651d8eb888bae64bd5ecfc34ae79ee
Bazztoner <bazztoner.phoenix@gmail.com>
2018-04-03 17:39:09 -0300

    3/4/18 => Update a las balas y prefabs de armas
    Prefabs de Armas ahora van en 0,0,0 del personaje (después vemos donde ponerlos)
    Las balas tienen su daño bien hecho
    Ya no se bugea el custom inspector

--- Assets/Script/Special Objects/Bullet.cs
+++ Assets/Script/Special Objects/Bullet.cs
@@ -10,10 +10,12 @@ public class Bullet : MonoBehaviour
     float MaxLifeTime { get { return maxLifeTime; } }
     float lifeTime = 0f;
     public float Lifetime { set { lifeTime = value; } }
+    float _travelledDistance;
+    public float DistanceTravelled { set { _travelledDistance = value; } }
     AnimationCurve _damageCurve;
     float _damage;
 
-    //Modifiqué los parámetros para que tome un daño mínimo y un daño máximo. 
+    //Modifiqué los parámetros para que tome un daño mínimo y un daño máximo, además de la curva de daño
     //Lo ideal sería heredar de Bullet y hacer balas para distinguir proyectil común, cargado y continuo.
     //De esta manera, podremos hacer el cálculo de daño actual (entre mínimo y máximo)
     public Bullet ConfigurateBullet(float speed, AnimationCurve curve, Vector3 position, Quaternion rotation, int emitter)
@@ -23,8 +25,6 @@ public class Bullet : MonoBehaviour
         gameObject.layer = emitter;
         transform.position = position;
         transform.rotation = rotation;
-
-        print("lacaca " + _damageCurve.Evaluate(34));
 
         return this;
     }
@@ -38,12 +38,20 @@ public class Bullet : MonoBehaviour
         }
         else
         {
-            transform.position += transform.forward * speed * Time.deltaTime;
-            //_damage = _damageCurve.Evaluate(lifeTime);
+            var distance = speed * Time.deltaTime;
+            _travelledDistance += distance;
+            transform.position += transform.forward * distance;
+            _damage = GetActualDamage(_travelledDistance);
+            print("DMG: " + _damage);
             //Acá tendrías que setear el daño actual al análisis del falloff de acuerdo al tiempo
             //que haya transcurrido dividido por el tiempo que se tardaría en completar el falloff.
             //Para esto tendrías que pedir velocidad, daño max y min, posición, rotación, emitter, curva de falloff y tiempo de duración del mismo al instanciar la bala.
         }
+    }
+
+    float GetActualDamage(float valueToAnalyze)
+    {
+        return _damageCurve.Evaluate(valueToAnalyze);
     }
 
     void OnTriggerEnter(Collider col)
@@ -58,6 +66,7 @@ public class Bullet : MonoBehaviour
     {
         bulletObj.gameObject.SetActive(true);
         bulletObj.Lifetime = 0f;
+        bulletObj.DistanceTravelled = 0f;
     }
 
     public static void DisposeBullet(Bullet bulletObj)
