ed80ff6c1763b6b2dd03344a749a4aebac0aef40
Daydam <dreamingagainmp@gmail.com>
2018-08-22 09:10:04 -0300

    Agregada rotación de cámara en Character Rotation
    - Se agregó un script de rotación de cámara alrededor de los personajes en Character Selection.
    - La cámara rota permanentemente. Tal vez en el futuro sea el jugador quien la controle.
    
    Adicional:
    - Se exportó a la carpeta Models la primera versión del escenario, sin UVs.
    - Agregado un FBX llamado Scene_Colliders, que, valga la reputísima redundancia, contiene los meshes para hacer el mesh collider del escenario.
    
    TODO:
    - Hacer que la rotación de cámara inicie cuando se spawnea el personaje.
    - Implementar el nuevo escenario, DE SER POSIBLE SIN NORMALES INVERTIDAS ♥

---
+++ Assets/Script/Character Selection/CameraRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    float rotationSpeed = 15;
+
+	void Update ()
+	{
+        transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime));
+	}
+}
