46f04bfdc8b2d65a8d06dc0aa97c8e49d734d982
Bazztoner <bazztoner.phoenix@gmail.com>
2018-08-30 00:09:19 -0300

    Lighting update
    Test de reflectores moviéndose
    Más balance al Assault Rifle

---
+++ Assets/Script/Testing/Reflectors.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Reflectors : MonoBehaviour
+{
+    public float speed;
+	
+	void Update ()
+	{
+        transform.Rotate(transform.up * speed * Time.deltaTime);
+	}
+}
