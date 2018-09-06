2052bbf6be5c5e7a0f7176f63175c30ca7d0c77c
Daydam <dreamingagainmp@gmail.com>
2018-09-05 23:52:00 -0300

    Agregadas partículas al EMPCloud
    - Agregado un sistema de partículas genérico para denotar dónde se encuentra la EMP Cloud.
    
    TODO:
    - Deshabilitar la destrucción de anillos, o hacer que se destruyan cuando la nube toque el borde interno. ESTO ES NECESARIO, AUNQUE SEA PROVISORIO, PARA AYUDAR A QUE EL JUEGO SEA INTUITIVO.

---
+++ Assets/Script/Special Objects/EmpCloudParticles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmpCloudParticles : MonoBehaviour
+{
+    ParticleSystem particles;
+
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    void Update ()
+	{
+        var asd = particles.shape;
+        asd.radius = transform.parent.lossyScale.x /2;
+	}
+}
