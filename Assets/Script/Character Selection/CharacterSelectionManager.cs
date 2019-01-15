b8885d5999821357e07e14caddd0b82d3184c892
Daydam <dreamingagainmp@gmail.com>
2019-01-15 14:34:32 -0300

    Character Selection Manager Changes
    Agregado código para seleccionar cuerpos en Character Selection. LOS TEXTOS SE VAN A CREAR EN EL PRÓXIMO PUSH.

--- Assets/Script/Character Selection/CharacterSelectionManager.cs
+++ Assets/Script/Character Selection/CharacterSelectionManager.cs
@@ -27,6 +27,7 @@ public class CharacterSelectionManager : MonoBehaviour
     
     public GameObject[] playerSpawnPoints;
     public GameObject[] blackScreens;
+    public Text[] bodyTexts;
     public Text[] weaponTexts;
     public Text[] defensiveTexts;
     public Text[] complementary1Texts;
@@ -38,10 +39,13 @@ public class CharacterSelectionManager : MonoBehaviour
     bool[] ready;
     public bool[] Ready { get { return ready; } }
     GameObject[] players;
+    GameObject[] currentBodies;
     GameObject[] currentWeapons;
     GameObject[,] currentComplementary;
     GameObject[] currentDefensive;
     CharacterURLs[] URLs;
+    List<GameObject> bodies;
+    int[] bodyIndexes;
     List<GameObject> weapons;
     int[] weaponIndexes;
     List<GameObject>[] complementarySkills;
@@ -49,7 +53,7 @@ public class CharacterSelectionManager : MonoBehaviour
     List<GameObject> defensiveSkills;
     int[] defensiveIndexes;
 
-    //A 4 index array that has values set for 0, 1, 2 or 3, 0 being weapon, 1 defensive, 2 first complementary, 3 second comp
+    //A 4 index array that has values set for 0, 1, 2, 3 or 4, 0 being body, 1 weapon, 2 defensive, 3 first complementary, 4 second comp
     int[] selectedModifier;
     public int[] SelectedModifier { get { return selectedModifier; } }
     Vector2[] lastAnalogValue;
@@ -68,6 +72,9 @@ public class CharacterSelectionManager : MonoBehaviour
         players = new GameObject[4];
         URLs = new CharacterURLs[4];
 
+        currentBodies = new GameObject[4];
+        bodyIndexes = new int[4] { 0, 0, 0, 0 };
+
         currentWeapons = new GameObject[4];
         weaponIndexes = new int[4] { 0, 0, 0, 0 };
 
@@ -79,6 +86,9 @@ public class CharacterSelectionManager : MonoBehaviour
 
         selectedModifier = new int[4] { 0, 0, 0, 0 };
         lastAnalogValue = new Vector2[4] { Vector2.zero, Vector2.zero, Vector2.zero, Vector2.zero };
+
+        bodies = new List<GameObject>();
+        bodies = Resources.LoadAll("Prefabs/CharacterSelection/Bodies").Select(x => (GameObject)x).ToList();
 
         weapons = new List<GameObject>();
         weapons = Resources.LoadAll("Prefabs/CharacterSelection/Weapons").Select(x => (GameObject)x).ToList();
@@ -118,10 +128,11 @@ public class CharacterSelectionManager : MonoBehaviour
                 if (!ready[i])
                 {
                     CheckSelect(i);
-                    if (selectedModifier[i] == 0) SelectWeapon(i);
-                    if (selectedModifier[i] == 1) SelectDefensive(i);
-                    if (selectedModifier[i] == 2) SelectComplementary(i, 0);
-                    if (selectedModifier[i] == 3) SelectComplementary(i, 1);
+                    if (selectedModifier[i] == 0) SelectBody(i);
+                    if (selectedModifier[i] == 1) SelectWeapon(i);
+                    if (selectedModifier[i] == 2) SelectDefensive(i);
+                    if (selectedModifier[i] == 3) SelectComplementary(i, 0);
+                    if (selectedModifier[i] == 4) SelectComplementary(i, 1);
                 }
                 lastAnalogValue[i] = JoystickInput.LeftAnalog(currentGamePads[i]);
 
@@ -134,6 +145,8 @@ public class CharacterSelectionManager : MonoBehaviour
                     if (ready[i])
                     {
                         //Set the text!
+                        var finalBody = bodies[bodyIndexes[i]].gameObject.name;
+                        bodyTexts[i].text = "Body: " + finalBody;
                         var finalWeapon = weapons[weaponIndexes[i]].gameObject.name;
                         weaponTexts[i].text = "Weapon: " + finalWeapon;
                         var finalDefensive = defensiveSkills[defensiveIndexes[i]].gameObject.name;
@@ -193,6 +206,9 @@ public class CharacterSelectionManager : MonoBehaviour
             {
                 URLs[player] = new CharacterURLs();
 
+                var bodyNameChars = bodies[bodyIndexes[player]].gameObject.name.TakeWhile(a => a != '(').ToArray();
+                string bodyName = new string(bodyNameChars);
+
                 var weaponNameChars = weapons[weaponIndexes[player]].gameObject.name.TakeWhile(a => a != '(').ToArray();
                 string weaponName = new string(weaponNameChars);
 
@@ -204,12 +220,14 @@ public class CharacterSelectionManager : MonoBehaviour
 
                 var defNameChars = defensiveSkills[defensiveIndexes[player]].gameObject.name.TakeWhile(a => a != '(').ToArray();
                 string defName = new string(defNameChars);
-                URLs[player].bodyURL = "Beetledrone";
+
+                URLs[player].bodyURL = bodyName;
                 URLs[player].weaponURL = weaponName;
                 URLs[player].complementaryURL = new string[2] { compName1, compName2 };
                 URLs[player].defensiveURL = defName;
             }
 
+            bodyIndexes[player] = bodies.IndexOf(Resources.Load<GameObject>("Prefabs/CharacterSelection/Bodies/" + URLs[player].bodyURL));
             weaponIndexes[player] = weapons.IndexOf(Resources.Load<GameObject>("Prefabs/CharacterSelection/Weapons/" + URLs[player].weaponURL));
             complementaryIndexes[player, 0] = complementarySkills[0].IndexOf(Resources.Load<GameObject>("Prefabs/CharacterSelection/Skills/Complementary 1/" + URLs[player].complementaryURL[0]));
             complementaryIndexes[player, 1] = complementarySkills[1].IndexOf(Resources.Load<GameObject>("Prefabs/CharacterSelection/Skills/Complementary 2/" + URLs[player].complementaryURL[1]));
@@ -224,9 +242,10 @@ public class CharacterSelectionManager : MonoBehaviour
             CharacterAssembler.Assemble(players[player], currentDefensive[player], currentComplementary[player, 0], currentComplementary[player, 1], currentWeapons[player]);
             players[player].GetComponentsInChildren<Renderer>().Where(x => x.material.GetTag("SkillStateColor", true, "Nothing") != "Nothing").First().material.SetColor("_PlayerColor", playerColors[player]);
 
-
+            var finalBody = bodies[bodyIndexes[player]].gameObject.name;
+            bodyTexts[player].text = "<   Body: " + finalBody + "   >";
             var finalWeapon = weapons[weaponIndexes[player]].gameObject.name;
-            weaponTexts[player].text = "<   Weapon: " + finalWeapon + "   >";
+            weaponTexts[player].text = "Weapon: " + finalWeapon;
             var finalDefensive = defensiveSkills[defensiveIndexes[player]].gameObject.name;
             defensiveTexts[player].text = "Defensive: " + finalDefensive;
             var finalComplementary1 = complementarySkills[0][complementaryIndexes[player, 0]].gameObject.name;
@@ -245,19 +264,73 @@ public class CharacterSelectionManager : MonoBehaviour
             if (JoystickInput.LeftAnalog(currentGamePads[player]).y <= -0.3f
             || JoystickInput.allKeys[JoystickKey.DPAD_DOWN](previousGamePads[player], currentGamePads[player]))
             {
-                selectedModifier[player] = selectedModifier[player] + 1 > 3 ? 0 : selectedModifier[player] + 1;
+                selectedModifier[player] = selectedModifier[player] + 1 > 4 ? 0 : selectedModifier[player] + 1;
             }
 
             if (JoystickInput.LeftAnalog(currentGamePads[player]).y >= 0.3f
             || JoystickInput.allKeys[JoystickKey.DPAD_UP](previousGamePads[player], currentGamePads[player]))
             {
-                selectedModifier[player] = selectedModifier[player] - 1 < 0 ? 3 : selectedModifier[player] - 1;
+                selectedModifier[player] = selectedModifier[player] - 1 < 0 ? 4 : selectedModifier[player] - 1;
             }
         }
 
         if (JoystickInput.allKeys[JoystickKey.B](previousGamePads[player], currentGamePads[player])
             || JoystickInput.allKeys[JoystickKey.BACK](previousGamePads[player], currentGamePads[player]))
             CancelPlayer(player);
+    }
+
+    void SelectBody(int player)
+    {
+        if (-0.3f < lastAnalogValue[player].x && lastAnalogValue[player].x < 0.3f)
+        {
+            if (JoystickInput.LeftAnalog(currentGamePads[player]).x >= 0.3f
+            || JoystickInput.allKeys[JoystickKey.DPAD_RIGHT](previousGamePads[player], currentGamePads[player]))
+            {
+                bodyIndexes[player]++;
+                if (bodyIndexes[player] >= bodies.Count) bodyIndexes[player] = 0;
+
+                var bodyName = bodies[bodyIndexes[player]].gameObject.name;
+                URLs[player].bodyURL = bodyName;
+                
+                currentBodies[player] = CharacterAssembler.ChangeBody(
+                    Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSelection/Bodies/" + URLs[player].bodyURL), players[player].transform.position, Quaternion.identity), 
+                    currentBodies[player], 
+                    currentDefensive[player], 
+                    currentComplementary[player, 0], 
+                    currentComplementary[player, 1], 
+                    currentWeapons[player]);
+            }
+
+            if (JoystickInput.LeftAnalog(currentGamePads[player]).x <= -0.3f
+            || JoystickInput.allKeys[JoystickKey.DPAD_LEFT](previousGamePads[player], currentGamePads[player]))
+            {
+                bodyIndexes[player]--;
+                if (bodyIndexes[player] < 0) bodyIndexes[player] = bodies.Count - 1;
+
+                var bodyName = bodies[bodyIndexes[player]].gameObject.name;
+                URLs[player].bodyURL = bodyName;
+
+                currentBodies[player] = CharacterAssembler.ChangeBody(
+                    Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSelection/Bodies/" + URLs[player].bodyURL), players[player].transform.position, Quaternion.identity),
+                    currentBodies[player],
+                    currentDefensive[player],
+                    currentComplementary[player, 0],
+                    currentComplementary[player, 1],
+                    currentWeapons[player]);
+            }
+        }
+
+        var finalBody = bodies[bodyIndexes[player]].gameObject.name;
+        bodyTexts[player].text = "<   Body: " + finalBody + "   >";
+        var finalWeapon = weapons[weaponIndexes[player]].gameObject.name;
+        weaponTexts[player].text = "Weapon: " + finalWeapon;
+        var finalDefensive = defensiveSkills[defensiveIndexes[player]].gameObject.name;
+        defensiveTexts[player].text = "Defensive: " + finalDefensive;
+        var finalComplementary1 = complementarySkills[0][complementaryIndexes[player, 0]].gameObject.name;
+        complementary1Texts[player].text = "Skill 1: " + finalComplementary1;
+        var finalComplementary2 = complementarySkills[1][complementaryIndexes[player, 1]].gameObject.name;
+        complementary2Texts[player].text = "Skill 2: " + finalComplementary2;
+
     }
 
     void SelectWeapon(int player)
@@ -291,6 +364,8 @@ public class CharacterSelectionManager : MonoBehaviour
             }
         }
 
+        var finalBody = bodies[bodyIndexes[player]].gameObject.name;
+        bodyTexts[player].text = "Body: " + finalBody;
         var finalWeapon = weapons[weaponIndexes[player]].gameObject.name;
         weaponTexts[player].text = "<   Weapon: " + finalWeapon + "   >";
         var finalDefensive = defensiveSkills[defensiveIndexes[player]].gameObject.name;
@@ -339,6 +414,8 @@ public class CharacterSelectionManager : MonoBehaviour
             }
         }
 
+        var finalBody = bodies[bodyIndexes[player]].gameObject.name;
+        bodyTexts[player].text = "Body: " + finalBody;
         var finalWeapon = weapons[weaponIndexes[player]].gameObject.name;
         weaponTexts[player].text = "Weapon: " + finalWeapon;
         var finalDefensive = defensiveSkills[defensiveIndexes[player]].gameObject.name;
@@ -390,6 +467,8 @@ public class CharacterSelectionManager : MonoBehaviour
             }
         }
 
+        var finalBody = bodies[bodyIndexes[player]].gameObject.name;
+        bodyTexts[player].text = "Body: " + finalBody;
         var finalWeapon = weapons[weaponIndexes[player]].gameObject.name;
         weaponTexts[player].text = "Weapon: " + finalWeapon;
         var finalDefensive = defensiveSkills[defensiveIndexes[player]].gameObject.name;
