0e3f6c74bd78b6132377d98b4ae61df9b09d0451
Daydam <31391172+Daydam@users.noreply.github.com>
2019-05-03 20:35:15 -0300

    Implementado el teclado en Controller
    Implementado el uso del teclado y mouse para jugar. ESTO SÓLO AFECTA AL JUGADOR 4.
    
    Controles:
    - Mouse: apuntar.
    - WASD: Moverse.
    - Q: Complementaria 1.
    - E: Complementaria 2.
    - Click Izquierdo: Armas.
    - Click Derecho: Defensiva.

--- Assets/Script/Controllers/Controller.cs
+++ Assets/Script/Controllers/Controller.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using XInputDotNetPure;
 
 public class Controller
 {
+    //Goes from 0 to 3!
     PlayerIndex player;
     GamePadState state;
     GamePadState prevState;
+    bool keyboardEnabled;
 
     /* In case I needed a 3D camera, I'd use this. If the camera is set to be top-down, then this should be disabled
     bool invertVertical;
@@ -34,6 +36,11 @@ public class Controller
         //XInput
         prevState = state;
         state = GamePad.GetState(player);
+        if((int)player == 3)
+        {
+            if (Input.GetKey(KeyCode.F) && Input.GetKey(KeyCode.Return)) keyboardEnabled = true;
+            if (Input.GetKey(KeyCode.F) && Input.GetKey(KeyCode.Escape)) keyboardEnabled = false;
+        }
     }
 
     public void SetVibration(float lowFrequencyIntensity, float highFrequencyIntensity)
@@ -43,32 +50,46 @@ public class Controller
 
     public Vector2 LeftAnalog()
     {
+        if(keyboardEnabled)
+        {
+            int xValue = 0;
+            if (Input.GetKey(KeyCode.A)) value--;
+            if (Input.GetKey(KeyCode.D)) value++;
+            int yValue = 0;
+            if (Input.GetKey(KeyCode.S)) value--;
+            if (Input.GetKey(KeyCode.W)) value++;
+            return new Vector2(xValue, yValue);
+        }
         return JoystickInput.LeftAnalog(state);
     }
 
     public Vector2 RightAnalog()
     {
-        return JoystickInput.RightAnalog(state);
+        if (keyboardEnabled) return Input.GetAxis("Mouse X");
+        else return JoystickInput.RightAnalog(state);
     }
 
     public bool MainWeapon()
     {
-
-        return JoystickInput.allKeys[mainWeapon](prevState, state);
+        if (keyboardEnabled) return Input.GetMouseButtonDown(0);
+        else return JoystickInput.allKeys[mainWeapon](prevState, state);
     }
 
     public bool DefensiveSkill()
     {
-        return JoystickInput.allKeys[defensiveSkill](prevState, state);
+        if (keyboardEnabled) return Input.GetMouseButtonDown(1);
+        else return JoystickInput.allKeys[defensiveSkill](prevState, state);
     }
 
     public bool ComplimentarySkill1()
     {
-        return JoystickInput.allKeys[complimentarySkill1](prevState, state);
+        if (keyboardEnabled) return Input.GetKeyDown(KeyCode.Q);
+        else return JoystickInput.allKeys[complimentarySkill1](prevState, state);
     }
 
     public bool ComplimentarySkill2()
     {
-        return JoystickInput.allKeys[complimentarySkill2](prevState, state);
+        if (keyboardEnabled) return Input.GetKeyDown(KeyCode.E);
+        else return JoystickInput.allKeys[complimentarySkill2](prevState, state);
     }
 }
