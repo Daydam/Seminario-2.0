ece02235585a49b7e5f29b1e412cbff0551277cf
Bazztoner <bazztoner.phoenix@gmail.com>
2019-05-07 15:14:05 -0300

    Art mini update
    Agregado nuevo modelo de Scrambler Bot.
    Agregado plugin de Substance para Unity (Juan slowly tilting)
    Agregada sustancia de oro para landmarks centrales.
    Agregada sustancia de arena para los pisos.
    SE VIENE EL RAYFIRE
    DE TU GOBIERNO
    DE MI GITARRA

--- Assets/Script/Framework/EndgameManager.cs
+++ Assets/Script/Framework/EndgameManager.cs
@@ -90,8 +90,8 @@ public class EndgameManager : MonoBehaviour
             //Dejo los objetos como children del body por cuestiones de carga de los scripts. Assembler no debería generar problemas, ya que su parent objetivo sería el mismo.
             var player = Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSelection/Bodies/" + URLs.bodyURL), spawnPos[i].position, Quaternion.identity);
             var weapon = Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSelection/Weapons/" + URLs.weaponURL), player.transform.position, Quaternion.identity, player.transform);
-            var comp1 = Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSelection/Skills/Complementary 1/" + URLs.complementaryURL[0]), player.transform.position, Quaternion.identity, player.transform);
-            var comp2 = Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSelection/Skills/Complementary 2/" + URLs.complementaryURL[1]), player.transform.position, Quaternion.identity, player.transform);
+            var comp1 = Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSelection/Skills/Complementary/" + URLs.complementaryURL[0]), player.transform.position, Quaternion.identity, player.transform);
+            var comp2 = Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSelection/Skills/Complementary/" + URLs.complementaryURL[1]), player.transform.position, Quaternion.identity, player.transform);
             var def = Instantiate(Resources.Load<GameObject>("Prefabs/CharacterSelection/Skills/Defensive/" + URLs.defensiveURL), player.transform.position, Quaternion.identity, player.transform);
             var tx = Instantiate(playerText, player.transform.position, Quaternion.identity, player.transform);
 
