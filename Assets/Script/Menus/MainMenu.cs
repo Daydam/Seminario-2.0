ad831c78306ff82dbf0e5ac1b6bdc2ec49565da3
Daydam <31391172+Daydam@users.noreply.github.com>
2020-01-21 13:42:12 -0300

    Button fixes
    Finally made the "Buttons" work in the main menu.
    
    Implemented a UIButton class, that isn't actually a button. Tbh, the "local/online play" functions are so hardcoded it hurts. Butt fuck it.

--- Assets/Script/Menus/MainMenu.cs
+++ Assets/Script/Menus/MainMenu.cs
@@ -11,8 +11,8 @@ public class MainMenu : MonoBehaviour
     GamePadState[] previousGamePads;
     GamePadState[] currentGamePads;
 
-    public Button localPlay;
-    public Button onlinePlay;
+    public UIButton[] buttons;
+    int currentButton = 0;
 
     void Start ()
 	{
@@ -22,40 +22,57 @@ public class MainMenu : MonoBehaviour
         {
             currentGamePads[i] = GamePad.GetState((PlayerIndex)i);
         }
-
+        buttons[currentButton].SetHighlighted(true);
         //Maybe check if you're connected to internet, and enable/disable the Online Button. MAYBE.
     }
 
     void Update()
     {
+        print(currentButton);
         for (int i = 0; i < currentGamePads.Length; i++)
         {
             previousGamePads[i] = currentGamePads[i];
             currentGamePads[i] = GamePad.GetState((PlayerIndex)i);
-            if(-0.3f < JoystickInput.LeftAnalog(currentGamePads[i]).y && JoystickInput.LeftAnalog(currentGamePads[i]).y < 0.3f)
+            if (-0.3f < JoystickInput.LeftAnalog(currentGamePads[i]).y && JoystickInput.LeftAnalog(currentGamePads[i]).y < 0.3f)
             {
                 if (JoystickInput.LeftAnalog(currentGamePads[i]).y <= -0.3f)
                 {
-                    //Select first button. Do we simply change colors and say "Fuck it?" Yeah, that works.
+                    buttons[currentButton].SetHighlighted(false);
+                    currentButton = currentButton == buttons.Length - 1 ? 0 : currentButton++;
+                    buttons[currentButton].SetHighlighted(true);
                 }
                 else if (JoystickInput.LeftAnalog(currentGamePads[i]).y >= 0.3f)
                 {
-
+                    buttons[currentButton].SetHighlighted(false);
+                    currentButton = currentButton == 0 ? buttons.Length - 1 : currentButton--;
+                    buttons[currentButton].SetHighlighted(true);
                 }
             }
-            if(Input.GetKeyDown(KeyCode.DownArrow))
+            if (JoystickInput.allKeys[JoystickKey.A](previousGamePads[i], currentGamePads[i])
+            || JoystickInput.allKeys[JoystickKey.START](previousGamePads[i], currentGamePads[i]))
             {
-
+                buttons[currentButton].SetPressed(true);
             }
-            else if(Input.GetKeyDown(KeyCode.UpArrow))
-            {
-
-            }
-            else if(Input.GetKeyDown(KeyCode.Return) || (JoystickInput.allKeys[JoystickKey.A](previousGamePads[i], currentGamePads[i])
-            || JoystickInput.allKeys[JoystickKey.START](previousGamePads[i], currentGamePads[i])))
-            {
-
-            }
+        }
+        //Keyboard
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            buttons[currentButton].SetHighlighted(false);
+            currentButton = Mathf.Min(currentButton+1, buttons.Length-1);
+            buttons[currentButton].SetHighlighted(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            buttons[currentButton].SetHighlighted(false);
+            currentButton = Mathf.Max(currentButton-1, 0);
+            buttons[currentButton].SetHighlighted(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            buttons[currentButton].SetPressed(true);
+            //The most hardcoded hardcode ever hardcoded
+            if (currentButton == 0) OnPlayLocal();
+            else if (currentButton == 1) OnPlayOnline();
         }
     }
 
---
+++ Assets/Script/Menus/UIButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButton : MonoBehaviour
+{
+    Image imgComp;
+    public Color regularColor = new Color(0.9f, 0.9f, 0.9f);
+    public Color highlightedColor = new Color(0.7f,0.7f,0.7f);
+    public Color clickedColor = new Color(0.3f,0.3f,0.3f);
+
+    private void Awake()
+    {
+        imgComp = GetComponent<Image>();
+    }
+
+    public void SetHighlighted(bool state)
+    {
+        if (state) imgComp.color = highlightedColor;
+        else imgComp.color = regularColor;
+    }
+
+    public void SetPressed(bool state)
+    {
+        if (state) imgComp.color = clickedColor;
+        else imgComp.color = highlightedColor;
+    }
+    
+}
