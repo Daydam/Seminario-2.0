0436ea4f562f4684dea1e025292444cdca029e08
Daydam <31391172+Daydam@users.noreply.github.com>
2020-06-16 18:53:12 -0300

    Update PlayerScoreController.cs
    PlayerScoreController; Add RPCSetSccore and RPCSetLeadingPlayer
    
    Pending initialization fixes;
    - NullReferenceException on PlayerUIModule line 18
    - ArgumentNullException on PlayerSightingHandler line 73

--- Assets/Script/Entities/Player/PlayerScoreController.cs
+++ Assets/Script/Entities/Player/PlayerScoreController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class PlayerScoreController : MonoBehaviour
 {
@@ -18,14 +19,34 @@ public class PlayerScoreController : MonoBehaviour
     public Text mainScore;
     Animator _an;
     Player _owner;
+    PhotonView pv;
 
     PlayerSightingHandler _sighting;
 
     void Start()
     {
+        if(!PhotonNetwork.InRoom || PhotonNetwork.InRoom && PhotonNetwork.IsMasterClient)
+        {
+            _owner = GetComponent<Player>();
+            var playerCount = Serializacion.LoadJsonFromDisk<RegisteredPlayers>("Registered Players").playerControllers.Length;
+            int playerIndex = GameManager.Instance.Players.IndexOf(GetComponent<Player>());
+            mainScore = GameObject.Find(playerCount.ToString() + " Player").transform.Find(canvasName).transform.Find("Player " + (playerIndex + 1)).GetComponentInChildren<Text>();
+            _an = mainScore.GetComponent<Animator>();
+
+            _sighting = GetComponent<PlayerSightingHandler>();
+
+            if (PhotonNetwork.InRoom && PhotonNetwork.IsMasterClient)
+            {
+                pv = GetComponent<PhotonView>();
+                pv.RPC("RPCStart", RpcTarget.Others, playerCount, playerIndex);
+            }
+        }
+    }
+
+    [PunRPC]
+    void RPCStart(int playerCount, int playerIndex)
+    {
         _owner = GetComponent<Player>();
-        var playerCount = Serializacion.LoadJsonFromDisk<RegisteredPlayers>("Registered Players").playerControllers.Length;
-        int playerIndex = GameManager.Instance.Players.IndexOf(GetComponent<Player>());
         mainScore = GameObject.Find(playerCount.ToString() + " Player").transform.Find(canvasName).transform.Find("Player " + (playerIndex + 1)).GetComponentInChildren<Text>();
         _an = mainScore.GetComponent<Animator>();
 
@@ -34,14 +55,18 @@ public class PlayerScoreController : MonoBehaviour
 
     public void SetScore(int main, int toAdd)
     {
-        mainScore.text = main.ToString();
+        if (!PhotonNetwork.InRoom || PhotonNetwork.InRoom && PhotonNetwork.IsMasterClient)
+        {
+            mainScore.text = main.ToString();
 
-        var toPlay = toAdd < 0 ? _removeState : toAdd > 0 ? _addState : _idleState;
+            var toPlay = toAdd < 0 ? _removeState : toAdd > 0 ? _addState : _idleState;
 
-        if (main == GameManager.Instance.GetScoreToWin()) toPlay = _maxScoreReachedState;
-        _an.Play(toPlay);
+            if (main == GameManager.Instance.GetScoreToWin()) toPlay = _maxScoreReachedState;
+            _an.Play(toPlay);
 
-        GameManager.Instance.ScoreUpdate();
+            GameManager.Instance.ScoreUpdate();
+
+        }
     }
 
     public void SetLeadingPlayer(bool activate)
